538aed7742cc9702e9f978ac9379c6d0879aff7d
Jason Moore <jason.moore1369@gmail.com>
2019-07-07 12:45:50 -0500

    removed unnecisary default files. Background image set

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        Lego Sith Memory
+      </header>
+      <hero className="App-hero">
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,10 +16,10 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           Learn React
         </a>
-      </header>
+      </hero>
     </div>
   );
 }
